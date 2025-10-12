4d4cf3e1775bd79c7903316ecdc438f1d281bd9d
i.raresandrei <i.raresandrei@yahoo.com>
2025-10-12 16:53:25 +0300

    remove package-lock from gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()] 
+  plugins: [react(), tailwindcss()] ,
+  base: "/uperpenta.github.io/",
 });
